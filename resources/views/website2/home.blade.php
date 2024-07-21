470630ce06c1d912fb4a52d19bae3ba1bcd812cf
fathysamir <01154857080fa@gmail.com>
2024-07-21 15:59:12 +0300

    Initial commit

--- resources/views/website2/home.blade.php
+++ resources/views/website2/home.blade.php
@@ -11,6 +11,7 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
+    <script src="{{asset('website/js/index.js')}}"></script>
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -279,7 +280,7 @@
         </div>
     </div>
 
-    <script src="{{asset('website/js/index.js')}}"></script>
+  
     <script src="https://code.jquery.com/jquery-1.10.2.js"></script>
     <script src="{{asset('website/js/stylish-portfolio.min.js')}}"></script>
 
