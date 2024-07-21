2ddbc1957ee99da3af06a7f17eb970880b68bd4f
fathysamir <01154857080fa@gmail.com>
2024-07-21 14:24:14 +0300

    Initial commit

--- resources/views/website2/home.blade.php
+++ resources/views/website2/home.blade.php
@@ -8,7 +8,7 @@
     <link rel="icon" type="image/x-icon" href="{{asset('website/assets/imgs/logo.png')}}" style="transform: scale(1.2);">
     <link rel="stylesheet" href="{{asset('website/css/style.css')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css">
     <link rel="stylesheet"
